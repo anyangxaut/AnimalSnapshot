80c5f27cea453a3dc722bbb630c3543a10d12b7e
anyangxaut <xautanyang@163.com>
2016-09-10 08:59:41 +0800

    Init project

---
+++ src/test/java/edu/xaut/animalsnapshot/AnimalSnapshotTest.java
@@ -0,0 +1,19 @@
+package edu.xaut.animalsnapshot;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by anyang on 2016/9/10.
+ */
+public class AnimalSnapshotTest {
+
+    private AnimalSnapshot animalSnapshot;
+
+    @Before
+    public void setUp() throws Exception {
+        animalSnapshot = new AnimalSnapshot();
+    }
+
+}
