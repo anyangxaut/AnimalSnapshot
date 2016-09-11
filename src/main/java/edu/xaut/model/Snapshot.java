eac26b97b33dee128437ee54a56293b1a169a58a
anyangxaut <xautanyang@163.com>
2016-09-11 19:17:52 +0800

    Implement SnapShot

--- src/main/java/edu/xaut/model/Snapshot.java
+++ src/main/java/edu/xaut/model/Snapshot.java
@@ -1,7 +1,30 @@
 package edu.xaut.model;
 
+import java.util.LinkedHashMap;
+
 /**
  * Created by anyang on 2016/9/10.
  */
 public class Snapshot {
+    private String snapshotId;
+    private String snapshotTime;
+    private LinkedHashMap<String, Animal> animals;
+
+    public Snapshot(String snapshotId, String snapshotTime, LinkedHashMap<String, Animal> animals) {
+        this.snapshotId = snapshotId;
+        this.snapshotTime = snapshotTime;
+        this.animals = animals;
+    }
+
+    public String getSnapshotId() {
+        return snapshotId;
+    }
+
+    public String getSnapshotTime() {
+        return snapshotTime;
+    }
+
+    public LinkedHashMap<String, Animal> getAnimals() {
+        return animals;
+    }
 }
