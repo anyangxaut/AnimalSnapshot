6c20ab32fffc4aef163e4e4b7a36cf31a3a7cae4
anyangxaut <xautanyang@163.com>
2016-09-11 23:22:57 +0800

    Change historyData and expected result in testGetSnapShotWithId3 method

--- src/test/java/edu/xaut/animalsnapshot/AnimalSnapshotTest.java
+++ src/test/java/edu/xaut/animalsnapshot/AnimalSnapshotTest.java
@@ -60,19 +60,19 @@ public class AnimalSnapshotTest {
     public void testGetSnapShotWithId3() throws Exception {
         final String historyData = "e4e87cb2-8e9a-4749-abb6-26c59344dfee\n" +
                 "2016/09/02 22:30:46\n" +
-                "cat1 10 9\n\n" +
+                "cat2 10 9\n\n" +
                 "351055db-33e6-4f9b-bfe1-16f1ac446ac1\n" +
                 "2016/09/02 22:30:52\n" +
-                "cat1 10 9 2 -1\n" +
-                "cat2 2 3\n\n" +
+                "cat2 10 9 2 -1\n" +
+                "cat1 2 3\n\n" +
                 "dcfa0c7a-5855-4ed2-bc8c-4accae8bd155\n" +
                 "2016/09/02 22:31:02\n" +
-                "cat1 12 8 3 4";
+                "cat2 12 8 3 4";
         final String id = "dcfa0c7a-5855-4ed2-bc8c-4accae8bd155";
 
         String snapshotInSpecifiedTime = animalSnapshot.getSnapShot(historyData, id);
 
-        assertEquals("cat1 15 12\ncat2 2 3\n", snapshotInSpecifiedTime);
+        assertEquals("cat1 2 3\ncat2 15 12\n", snapshotInSpecifiedTime);
     }
 
     @Test
