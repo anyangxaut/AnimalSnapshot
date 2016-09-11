a7b8eca303955608c41e754479ed1eeaa060dc81
anyangxaut <xautanyang@163.com>
2016-09-11 19:41:11 +0800

    implement InputVerification and InputVerificationTest

--- src/main/java/edu/xaut/animalsnapshot/InputVerification.java
+++ src/main/java/edu/xaut/animalsnapshot/InputVerification.java
@@ -1,7 +1,54 @@
 package edu.xaut.animalsnapshot;
 
+import edu.xaut.exception.ConflictFoundException;
+import edu.xaut.exception.InvalidFormatException;
+import edu.xaut.model.Animal;
+
 /**
  * Created by anyang on 2016/9/10.
  */
 public class InputVerification {
+    private String idValidRegex = "^[A-Za-z0-9]{8}-[A-Za-z0-9]{4}-[A-Za-z0-9]{4}-[A-Za-z0-9]{4}-[A-Za-z0-9]{12}$";
+    private String timeValidRegex = "^\\d{4}/\\d{2}/\\d{2} \\d{2}:\\d{2}:\\d{2}$";
+
+    public String isSnapshotIdValid(String snapshotId) throws InvalidFormatException {
+        if (!snapshotId.matches(idValidRegex)) {
+            throw new InvalidFormatException("Invalid format for SnapshotId.");
+        }
+        return snapshotId;
+    }
+
+    public String isSnapshotTimeValid(String snapshotTime) throws InvalidFormatException {
+        if (!snapshotTime.matches(timeValidRegex)) {
+            throw new InvalidFormatException("Invalid format for SnapshotTime.");
+        }
+        return snapshotTime;
+    }
+
+    public String isAnimalCoordinatesValid(String animalCoordinates) throws InvalidFormatException {
+        String[] informations = animalCoordinates.split(" ");
+        if (informations.length != 3 && informations.length != 5) {
+            throw new InvalidFormatException("Invalid format for AnimalCoordinates.");
+        }
+        return animalCoordinates;
+    }
+
+    public boolean isCoordinatesConflict(Animal lastInputAnimal, Animal currentInputAnimal) throws ConflictFoundException {
+        boolean xCoordinateConflict = isExsitConflictData(lastInputAnimal.getxCoordinateInCurrentTime(),
+                currentInputAnimal.getxCoordinate());
+        boolean yCoordinateConflict = isExsitConflictData(lastInputAnimal.getyCoordinateInCurrentTime(),
+                currentInputAnimal.getyCoordinate());
+        if (xCoordinateConflict || yCoordinateConflict){
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isExsitConflictData(int expected, int actual) {
+        if (expected == actual) {
+            return false;
+        }else {
+            return true;
+        }
+    }
 }
--- src/test/java/edu/xaut/animalsnapshot/InputVerificationTest.java
+++ src/test/java/edu/xaut/animalsnapshot/InputVerificationTest.java
@@ -1,8 +1,159 @@
-import static org.junit.Assert.*;
+package edu.xaut.animalsnapshot;
+
+import edu.xaut.model.Animal;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by anyang on 2016/9/10.
  */
 public class InputVerificationTest {
 
+    private InputVerification inputVerification;
+
+    @Before
+    public void setUp() throws Exception {
+        inputVerification = new InputVerification();
+    }
+
+    @Test
+    public void testIsSnapshotIdValidWhenIdIsValid() throws Exception {
+        String expectedId = "e4e87cb2-8e9a-4749-abb6-26c59344dfee";
+
+        String actualId = inputVerification.isSnapshotIdValid(expectedId);
+
+        assertEquals(expectedId, actualId);
+    }
+
+    @Test (expected = edu.xaut.exception.InvalidFormatException.class)
+    public void testIsSnapshotIdValidWhenIdIsNotValidWithBlank() throws Exception {
+        String expectedId = "e4e87cb2-8e   9a-4749-abb6-26c59344dfee";
+
+        String actualId = inputVerification.isSnapshotIdValid(expectedId);
+
+        assertEquals(expectedId, actualId);
+    }
+
+    @Test (expected = edu.xaut.exception.InvalidFormatException.class)
+    public void testIsSnapshotIdValidWhenIdIsNotValidWithEmpty() throws Exception {
+        String expectedId = "";
+
+        String actualId = inputVerification.isSnapshotIdValid(expectedId);
+
+        assertEquals(expectedId, actualId);
+    }
+
+    @Test
+    public void testIsSnapshotTimeValidWhenTimeIsValid() throws Exception {
+        String expectedTime = "2016/09/02 22:30:46";
+
+        String actualTime = inputVerification.isSnapshotTimeValid(expectedTime);
+
+        assertEquals(expectedTime, actualTime);
+    }
+
+    @Test (expected = edu.xaut.exception.InvalidFormatException.class)
+    public void testIsSnapshotTimeValidWhenTimeIsNotValidWithIllegalCharacter() throws Exception {
+        String expectedTime = "201P/09/S2 22:30:46";
+
+        String actualTime = inputVerification.isSnapshotIdValid(expectedTime);
+
+        assertEquals(expectedTime, actualTime);
+    }
+
+    @Test (expected = edu.xaut.exception.InvalidFormatException.class)
+    public void testIsSnapshotTimeValidWhenTimeIsNotValidWithEmpty() throws Exception {
+        String expectedTime = "";
+
+        String actualTime = inputVerification.isSnapshotIdValid(expectedTime);
+
+        assertEquals(expectedTime, actualTime);
+    }
+
+    @Test
+    public void testIsAnimalCoordinatesValidWhenCoordinatesIsValidAndLengthIsThree() throws Exception {
+        String expectedCoordinates = "cat1 10 9";
+
+        String actualCoordinates = inputVerification.isAnimalCoordinatesValid(expectedCoordinates);
+
+        assertEquals(expectedCoordinates, actualCoordinates);
+    }
+
+    @Test
+    public void testIsAnimalCoordinatesValidWhenCoordinatesIsValidAndLengthIsFive() throws Exception {
+        String expectedCoordinates = "cat1 12 8 3 4";
+
+        String actualCoordinates = inputVerification.isAnimalCoordinatesValid(expectedCoordinates);
+
+        assertEquals(expectedCoordinates, actualCoordinates);
+    }
+
+    @Test (expected = edu.xaut.exception.InvalidFormatException.class)
+    public void testIsAnimalCoordinatesValidWhenCoordinatesIsNotValid() throws Exception {
+        String expectedCoordinates = "cat1 12 8 3fgfhgh";
+
+        String actualCoordinates = inputVerification.isAnimalCoordinatesValid(expectedCoordinates);
+
+        assertEquals(expectedCoordinates, actualCoordinates);
+    }
+
+    @Test
+    public void testIsCoordinatesConflictWhenCoordinatesAreEqual() throws Exception {
+        Animal lastInputAnimal = mock(Animal.class);
+        when(lastInputAnimal.getxCoordinateInCurrentTime()).thenReturn(2);
+        when(lastInputAnimal.getyCoordinateInCurrentTime()).thenReturn(5);
+        Animal currentInputAnimal = mock(Animal.class);
+        when(currentInputAnimal.getxCoordinate()).thenReturn(2);
+        when(currentInputAnimal.getyCoordinate()).thenReturn(5);
+
+        boolean isConflict = inputVerification.isCoordinatesConflict(lastInputAnimal, currentInputAnimal);
+
+        assertEquals(false, isConflict);
+    }
+
+    @Test
+    public void testIsCoordinatesConflictWhenXCoordinateNotEqual() throws Exception {
+        Animal lastInputAnimal = mock(Animal.class);
+        when(lastInputAnimal.getxCoordinateInCurrentTime()).thenReturn(2);
+        when(lastInputAnimal.getyCoordinateInCurrentTime()).thenReturn(5);
+        Animal currentInputAnimal = mock(Animal.class);
+        when(currentInputAnimal.getxCoordinate()).thenReturn(8);
+        when(currentInputAnimal.getyCoordinate()).thenReturn(5);
+
+        boolean isConflict = inputVerification.isCoordinatesConflict(lastInputAnimal, currentInputAnimal);
+
+        assertEquals(true, isConflict);
+    }
+
+    @Test
+    public void testIsCoordinatesConflictWhenYCoordinateNotEqual() throws Exception {
+        Animal lastInputAnimal = mock(Animal.class);
+        when(lastInputAnimal.getxCoordinateInCurrentTime()).thenReturn(2);
+        when(lastInputAnimal.getyCoordinateInCurrentTime()).thenReturn(5);
+        Animal currentInputAnimal = mock(Animal.class);
+        when(currentInputAnimal.getxCoordinate()).thenReturn(2);
+        when(currentInputAnimal.getyCoordinate()).thenReturn(9);
+
+        boolean isConflict = inputVerification.isCoordinatesConflict(lastInputAnimal, currentInputAnimal);
+
+        assertEquals(true, isConflict);
+    }
+
+    @Test
+    public void testIsCoordinatesConflictWhenCoordinatesNotEqual() throws Exception {
+        Animal lastInputAnimal = mock(Animal.class);
+        when(lastInputAnimal.getxCoordinateInCurrentTime()).thenReturn(2);
+        when(lastInputAnimal.getyCoordinateInCurrentTime()).thenReturn(5);
+        Animal currentInputAnimal = mock(Animal.class);
+        when(currentInputAnimal.getxCoordinate()).thenReturn(3);
+        when(currentInputAnimal.getyCoordinate()).thenReturn(7);
+
+        boolean isConflict = inputVerification.isCoordinatesConflict(lastInputAnimal, currentInputAnimal);
+
+        assertEquals(true, isConflict);
+    }
 }
