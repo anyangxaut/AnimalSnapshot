b41228ff1c071659be4f0f040ee9a5379c94477d
anyangxaut <xautanyang@163.com>
2016-09-11 23:20:17 +0800

    Add sortWithAnimalId method

--- src/main/java/edu/xaut/animalsnapshot/SnapshotPrinter.java
+++ src/main/java/edu/xaut/animalsnapshot/SnapshotPrinter.java
@@ -3,8 +3,7 @@ package edu.xaut.animalsnapshot;
 import edu.xaut.model.Animal;
 import edu.xaut.model.Snapshot;
 
-import java.util.LinkedHashMap;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by anyang on 2016/9/10.
@@ -12,12 +11,25 @@ import java.util.List;
 public class SnapshotPrinter {
 
     public String printMultipleAnimalCoordinates(Snapshot snapshot) {
-        LinkedHashMap<String, Animal> animals = snapshot.getAnimals();
+        List<Animal> animals = sortWithAnimalId(snapshot.getAnimals());
         StringBuilder AnimalCoordinates = new StringBuilder();
-        for (String animalId : animals.keySet()) {
-            AnimalCoordinates.append(printEachAnimalCoordinates(animals.get(animalId)));
+        for (Animal animal : animals) {
+            AnimalCoordinates.append(printEachAnimalCoordinates(animal));
         }
         return AnimalCoordinates.toString();
+    }
+
+    public List<Animal> sortWithAnimalId(LinkedHashMap<String, Animal> animals) {
+        List<Animal> animalsList = new ArrayList<Animal>();
+        for (String animalId : animals.keySet()) {
+            animalsList.add(animals.get(animalId));
+        }
+        Collections.sort(animalsList, new Comparator<Animal>() {
+            public int compare(Animal o1, Animal o2) {
+                return o1.getAnimalId().compareTo(o2.getAnimalId());
+            }
+        });
+        return animalsList;
     }
 
     public String printEachAnimalCoordinates(Animal animal) {
